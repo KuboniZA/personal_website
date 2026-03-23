c8502ddb08c13d9c215e6efaf37c7414ef413f3f
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-03-23 21:37:54 +0200

    Add animation to highlight skills. Current colour is blue.

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
  document.addEventListener('DOMContentLoaded', () => { 
-    const images = document.querySelectorAll('.md-slides, .md-slidesB');
+    const images = document.querySelectorAll('.md-slides, .md-slidesB, .md-slides2, .md-slidesB2');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            if (entry.intersectionRatio === 1) {
+            if (entry.intersectionRatio >= 0.99) {
                 // Fully visible → start animation
                 entry.target.style.transform = "rotateY(180deg)";
                 
             } else {
                 // Not fully visible → pause
-                entry.target.style.animationPlayState = 'paused';
+                // entry.target.style.transform = "rotateY(360deg)";
             }
         });
     }, {
-        threshold: 1.0 // 100% visibility required
+        threshold: [0, 0.99]  // 100% visibility required
     });
 
     images.forEach(img => observer.observe(img));
+
+    
  });
