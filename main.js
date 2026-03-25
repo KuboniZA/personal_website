39b7abfe3bb590379eaa63be4d2f2a287915cc19
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-03-25 17:37:07 +0200

    Add reactive scroll popup, where up and down arrows are visible based on screen location (Javascript to add CCS cpmmand). Snapping also works fine.

--- main.js
+++ main.js
@@ -18,5 +18,34 @@
 
     images.forEach(img => observer.observe(img));
 
-    
+    // Hide and reveal the arrows ⬇
+    const mainContainer = document.querySelector('.site-container')
+    // mainContainer.scrollTop = 0;
+
+    const upArrow = document.querySelector('.up-arrow');
+    const downArrow = document.querySelector('.down-arrow');
+
+    function handleScroll() {
+        const scrollTop = mainContainer.scrollTop;
+        const viewportHeight = mainContainer.clientHeight;
+        const fullHeight = mainContainer.scrollHeight;
+
+        // Hide up arrow at top
+        if (scrollTop <= 0) {
+            upArrow.classList.add('hidden-arrows');
+        } else {
+            upArrow.classList.remove('hidden-arrows');
+        }
+
+        // Hide down arrow at bottom
+        if (scrollTop + viewportHeight >= fullHeight - 1) {
+            downArrow.classList.add('hidden-arrows');
+        } else {
+            downArrow.classList.remove('hidden-arrows');
+        }
+    }
+
+    mainContainer.addEventListener('scroll', handleScroll);
+    handleScroll(); // run once on load
+
  });
